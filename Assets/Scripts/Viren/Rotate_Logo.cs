db24e463ee56b5f0e14fa50866a142f5872afceb
AIV\1820001 <virenkullur66@gmail.com>
2018-12-06 16:08:39 -0800

    Added Images folder with background, logo, and rotating script etc.

---
+++ Assets/Scripts/Viren/Rotate_Logo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Logo : MonoBehaviour
+{
+    
+
+    public float spinSpeed = 10f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+    }
+}
